8629280e4b9c1f09e97431791665de3ed396969c
kosbotic <kosbotic@gmail.com>
2016-05-28 23:37:13 -0400

    initial commit

---
+++ myFirstTest/src/test/java/com/sk/LoginTest.java
@@ -0,0 +1,31 @@
+package com.sk;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class LoginTest {
+  @Test(dataProvider = "dp")
+  public void regularLoginIn(Integer n, String s) {
+	  System.out.println("-- Perfect login----> n="+n+" / s="+s);
+  }
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "Steven Koo / password" },
+      new Object[] { 2, "lucky Igho /password" },
+    };
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.out.println("before Test---");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  System.out.println("after Test---");
+  }
+
+}
