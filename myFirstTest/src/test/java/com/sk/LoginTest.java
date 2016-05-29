4a5bc13191f717457494b4b9f3c7758265455689
kosbotic <kosbotic@gmail.com>
2016-05-28 23:57:51 -0400

    slight changes

--- myFirstTest/src/test/java/com/sk/LoginTest.java
+++ myFirstTest/src/test/java/com/sk/LoginTest.java
@@ -14,8 +14,8 @@ public class LoginTest {
   @DataProvider
   public Object[][] dp() {
     return new Object[][] {
-      new Object[] { 1, "Steven Koo / password" },
-      new Object[] { 2, "lucky Igho /password" },
+      new Object[] { 1, "Steven Koo / password git" },
+      new Object[] { 2, "lucky Igho /password git" },
     };
   }
   @BeforeTest
